95444f507a0f115708c6d0c0e10682d69063e8ca
Mesto <xamlol2982002@gmail.com>
2022-09-08 17:50:22 +0700

    complete task of week 1

---
+++ src/Week_1/HelloWorld.java
@@ -0,0 +1,12 @@
+package src.Week_1;
+
+public class HelloWorld {
+    public static String printHello() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        String helloWorld = printHello();
+        System.out.println(helloWorld);
+    }
+}
